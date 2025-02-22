a20eae9c9c0739638fe3765cfef98a3a6412e194
FrankBonanno <fb0023@mix.wvu.edu>
2025-02-21 19:02:58 -0500

    upstash setup

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,15 @@
+const TooFastPage = () => {
+	return (
+		<main className="root-container flex min-h-screen flex-col items-center justify-center">
+			<h1 className="font-bebas-neue text-5xl font-bold text-center text-light-100">
+				Whoa, slow down there, speed racer!
+			</h1>
+			<p className="mt-3 max-w-xl text-center text-light-400">
+				Looks like you were a little too eager. 🚦 We have put a temporary hold on your excitement. Chill for a bit, and
+				try again.
+			</p>
+		</main>
+	);
+};
+
+export default TooFastPage;
