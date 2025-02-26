756b86b0c6d9ff694dba9eb73a3b15797c24fd88
FrankBonanno <fb0023@mix.wvu.edu>
2025-02-26 00:54:01 -0500

    fix redirect

--- components/BorrowBook.tsx
+++ components/BorrowBook.tsx
@@ -37,7 +37,7 @@ const BorrowBook = ({ userId, bookId, borrowingEligibility: { isEligible, messag
 					description: 'Book borrowed successfully.',
 				});
 
-				router.push('/my-profile');
+				router.push('/');
 			} else {
 				toast({
 					title: 'Error',
